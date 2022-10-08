2e435c184fd9a0e443df1d21f6a5a4b090718186
rachidassouani <rachidassouani@gmail.com>
2022-10-08 20:22:58 +0100

    Added Employee class in the model layer

---
+++ src/main/java/io/rachidassouani/model/Employee.java
@@ -0,0 +1,61 @@
+package io.rachidassouani.model;
+
+public class Employee {
+
+	private Long id;
+	private String fullName;
+	private String email;
+	private String gender;
+	private String hobbies;
+	private String country;
+	private String address;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getHobbies() {
+		return hobbies;
+	}
+	public void setHobbies(String hobbies) {
+		this.hobbies = hobbies;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", fullName=" + fullName + ", email=" + email + ", gender=" + gender
+				+ ", hobbies=" + hobbies + ", country=" + country + ", address=" + address + "]";
+	}
+}
